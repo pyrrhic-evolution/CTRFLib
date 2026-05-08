a5b6e8eefeaa223354cb1ff374c4961985631a81
Pyrrhic Evolution <pyrrhicevolution@gmail.com>
2026-05-07 19:46:43 -0700

    Add project files.

---
+++ include/CTRFLib.h
@@ -0,0 +1,8 @@
+﻿// CTRFLib.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
---
+++ include/types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace ctrf {
+	#pragma pack(push, 1)
+	typedef char ctrf_magic[4];
+
+	typedef struct {
+
+	} ctrf_header;
+	#pragma pack(pop)
+}
